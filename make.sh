777d577bf1c60b1b619ea7aedee1d805a04d36ec
weihua.liu <auhiewuil@gmail.com>
2018-06-27 22:56:35 +0800

    add makefile

--- make.sh
+++ make.sh
@@ -1,44 +1,40 @@
-##################################
-# Desc: 编译Cmake Files 产生静态库
-# Author: liu.weihua<394806487@qq.com>
-# Created Time: 06/24/2018
-##################################
-#!/bin/sh
+rm */*.o
+cd string
+make
+cd ../conf
+make
+cd ../ds
+make
+cd ../crypto
+make
+cd ../socket
+make
+cd ../sccli
+make
+cd ../scsrv
+make
 
-rm -rf build;
-mkdir build;
-cd build;
-cmake ../;
-make;
+if [ -d ../pack ]
+then
+cd ../pack
+make
+fi
 
-## libscbase.a
-mkdir libscbase;
-cp conf/libconf.a libscbase/;
-cp crypto/libcrypto.a libscbase/;
-cp ds/libds.a libscbase/;
-cp pack/libpack.a libscbase/;
-cd libscbase/;
-ar -x libconf.a;
-ar -x libcrypto.a;
-ar -x libds.a;
-ar -x libpack.a;
-ar -r libscbase.a *.o;
-mv libscbase.a ../;
-cd ../;
-rm -rf libscbase;
+if [ -d ../sqli ]
+then
+cd ../sqli
+make
+fi
 
-## libsc.a
-mkdir libsc;
-cp sccli/libsccli.a libsc/;
-cp scsrv/libscsrv.a libsc/;
-cp socket/libsocket.a libsc/;
-cd libsc/;
-ar -x libsccli.a;
-ar -x libscsrv.a;
-ar -x libsocket.a;
-ar -r libsc.a *.o;
-mv libsc.a ../;
-cd ../;
-rm -rf libsc;
+if [ -d ../dau ]
+then
+cd ../dau
+make
+fi
 
+if [ -d ../sdbc ]
+then
+cd ../sdbc
+make
+fi
 
